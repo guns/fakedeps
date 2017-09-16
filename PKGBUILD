bd79e53577bf4841578faa37f7c9398466a966b9
guns <self@sungpae.com>
2017-09-15 20:55:40 -0500

    Add libavahi-ui-gtk3.so

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 # Maintainer: Sung Pae <self@sungpae.com>
 pkgname=fakedeps
-pkgver=10
+pkgver=11
 pkgrel=1
 pkgdesc="A dummy package that satisfies unwanted dependencies."
 arch=('any')
@@ -26,6 +26,7 @@ package() {
     _package_lib libatk-bridge-2.0.c libatk-bridge-2.0.so{,.0,.0.0.0}
     _package_lib libavahi-common.c   libavahi-common.so{,.3,.3.5.3}
     _package_lib libavahi-client.c   libavahi-client.so{,.3,.3.2.9}
+    _package_lib libavahi-ui-gtk3.c  libavahi-ui-gtk3.so{,.0}
     _package_lib libcolord.c         libcolord.so{,.2,.2.0.5}
     _package_lib libcolord-gtk.c     libcolord-gtk.so{,.1,.1.0.3}
 }
---
+++ libavahi-ui-gtk3.c
@@ -0,0 +1,16 @@
+extern char *aui_service_dialog_get_host_name() { return 0; }
+extern int aui_service_dialog_get_type() { return 0; }
+extern int avahi_simple_poll_iterate() { return 0; }
+extern unsigned int aui_service_dialog_get_port() { return 0; }
+extern void *aui_service_dialog_new() { return 0; }
+extern void *avahi_service_browser_get_client() { return 0; }
+extern void *avahi_service_browser_new() { return 0; }
+extern void *avahi_service_resolver_new() { return 0; }
+extern void *avahi_simple_poll_get() { return 0; }
+extern void aui_service_dialog_set_browse_service_types() {}
+extern void aui_service_dialog_set_resolve_host_name() {}
+extern void aui_service_dialog_set_resolve_service() {}
+extern void avahi_service_browser_free() {}
+extern void avahi_service_resolver_free() {}
+extern void avahi_service_resolver_get_client() { return 0; }
+extern void avahi_simple_poll_free() {}
